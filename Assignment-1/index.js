bd18767297353e079464dd6f48b74fafa5501777
salman-a1212 <82266019+salman-a1212@users.noreply.github.com>
2021-10-07 10:22:47 +0500

    Update index.js

--- Assignment-1/index.js
+++ Assignment-1/index.js
@@ -1,8 +1,8 @@
-function myfunction() {
+btn.onclick = function myfunction() {
   let num1 = document.getElementById("num1").value;
   let num2 = document.getElementById("num2").value;
 
   let result = Number(num1) + Number(num2);
-  // console.log("result: ", result);
+  console.log("result: ", result);
   document.getElementById("result").innerText = result;
-}
+};
